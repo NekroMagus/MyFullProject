8ec69a85c029c0be4ff235fe103d5c1a7dbd7793
NekroMagus <nekromagus@mail.ru>
2019-08-01 22:22:31 +0300

    many changes

--- FootballManagerSite/src/main/java/com/football/manager/dao/VideoDao.java
+++ FootballManagerSite/src/main/java/com/football/manager/dao/VideoDao.java
@@ -1,7 +1,7 @@
-//package com.football.manager.dao;
-//
-//import com.football.manager.domain.Video;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface VideoDao extends JpaRepository<Video,Long> {
-//}
+package com.football.manager.dao;
+
+import com.football.manager.domain.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VideoDao extends JpaRepository<Video, Long> {
+}
--- FootballManagerSite/src/main/java/com/football/manager/service/VideoService.java
+++ FootballManagerSite/src/main/java/com/football/manager/service/VideoService.java
@@ -1,14 +1,14 @@
-//package com.football.manager.service;
-//
-//import com.football.manager.dao.VideoDao;
-//import com.football.manager.domain.Video;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//public class VideoService {
-//    @Autowired
-//    private VideoDao videoDao;
-//
-//    public void addVideo(Video video) {
-//        videoDao.save(video);
-//    }
-//}
+package com.football.manager.service;
+
+import com.football.manager.dao.VideoDao;
+import com.football.manager.domain.Video;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class VideoService {
+    @Autowired
+    private VideoDao videoDao;
+
+    public void addVideo(Video video) {
+        videoDao.save(video);
+    }
+}
