1465edf541836305d1dcdfbd410421bc93164cd8
NekroMagus <nekromagus@mail.ru>
2019-08-06 14:52:48 +0300

    add new permits

--- FootballManagerSite/src/main/java/com/football/manager/config/WebSecurityConfig.java
+++ FootballManagerSite/src/main/java/com/football/manager/config/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/registration", "/login", "/static/**").permitAll()
+                .antMatchers("/","/registration", "/login", "/static/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
