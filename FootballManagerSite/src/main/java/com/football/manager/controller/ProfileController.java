decc3e646c18573b16d1f20edf409ada13f590c1
NekroMagus <NekroMagus@mail.ru>
2019-09-18 19:48:02 +0300

    add Post Mapping

--- FootballManagerSite/src/main/java/com/football/manager/controller/ProfileController.java
+++ FootballManagerSite/src/main/java/com/football/manager/controller/ProfileController.java
@@ -1,7 +1,10 @@
 package com.football.manager.controller;
 
+import com.football.manager.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -22,5 +25,12 @@ public class ProfileController {
     @GetMapping
     public String getProfile(){
         return "profile";
+    }
+
+    @PostMapping
+    public String editProfile(@ModelAttribute("user") User user) {
+
+
+        return "redirect:/edit";
     }
 }
