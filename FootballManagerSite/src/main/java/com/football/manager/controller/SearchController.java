ea65404bd5a2f1da5512c41d07e2d06b2077367a
NekroMagus <NekroMagus@mail.ru>
2019-09-19 23:33:52 +0300

    some changes

--- FootballManagerSite/src/main/java/com/football/manager/controller/SearchController.java
+++ FootballManagerSite/src/main/java/com/football/manager/controller/SearchController.java
@@ -1,8 +1,15 @@
 package com.football.manager.controller;
 
+import com.football.manager.model.User;
+import com.football.manager.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /**
  * Controller for search page.
@@ -14,8 +21,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/search")
 public class SearchController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
     public String getSearch() {
+        return "search";
+    }
+
+    @GetMapping("/result")
+    public String getResultSearch(@RequestParam String found, Model model) {
+        List<User> userList = userService.findUsersByName(found);
+        model.addAttribute("userList",userList);
         return "search";
     }
 }
