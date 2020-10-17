99e6c633dd735187657b56ab65058e7b55bd4070
Opachi <egorodinczov@yandex.ru>
2020-10-17 20:39:15 +0300

    eureka client

--- player-service/src/main/java/net/skideo/SkideoApplication.java
+++ player-service/src/main/java/net/skideo/SkideoApplication.java
@@ -2,8 +2,10 @@ package net.skideo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SkideoApplication {
 
     public static void main(String[] args) {
