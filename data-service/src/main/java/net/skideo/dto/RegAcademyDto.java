5275992c462680a3b96106b19b30f2d7ef17eaef
Opachi <egorodinczov@yandex.ru>
2020-12-17 19:20:27 +0300

    authorization and registration changes(academy-service)

--- data-service/src/main/java/net/skideo/dto/RegAcademyDto.java
+++ data-service/src/main/java/net/skideo/dto/RegAcademyDto.java
@@ -2,13 +2,21 @@ package net.skideo.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class RegAcademyDto {
 
+    @Min(6)
     private String login;
+    @Min(6)
     private String password;
+    @NotNull
     private String title;
+    @NotNull
     private String city;
+    @NotNull
     private String country;
     private int numberPlayers;
 
