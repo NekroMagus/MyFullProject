32dac826136019a79a5ad00e9649811004bc9a79
Opachi <egorodinczov@yandex.ru>
2021-03-02 18:51:20 +0300

    refactoring

---
+++ notification-service/src/main/java/net/skideo/config/MailConfig.java
@@ -0,0 +1,41 @@
+package net.skideo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+import java.util.logging.Logger;
+
+@Configuration
+public class MailConfig {
+
+    @Value("${spring.email.username}")
+    private String username;
+
+    @Value("${spring.email.password}")
+    private String password;
+
+    Logger log = Logger.getLogger(MailConfig.class.getName());
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+}
--- notification-service/src/main/java/net/skideo/service/notification/NotificationServiceImpl.java
+++ notification-service/src/main/java/net/skideo/service/notification/NotificationServiceImpl.java
@@ -9,9 +9,12 @@ import net.skideo.model.enums.NotificationEnum;
 import net.skideo.repository.NotificationRepository;
 import net.skideo.service.academy.AcademyService;
 import net.skideo.service.user.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedList;
@@ -23,6 +26,10 @@ public class NotificationServiceImpl implements NotificationService {
     private final AcademyService academyService;
     private final UserService userService;
     private final NotificationRepository repository;
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.email.username}")
+    private String username;
 
     @Override
     public void addNotification(NotificationEnum notificationEnum, long idUser) {
@@ -36,6 +43,8 @@ public class NotificationServiceImpl implements NotificationService {
         user.getNotification().add(notification);
 
         userService.save(user);
+
+        sendNotification(user.getInfo().getEmail(),notificationEnum.getNotification() + " by " + currentAcademy.getInfo().getName());
     }
 
     @Override
@@ -44,6 +53,17 @@ public class NotificationServiceImpl implements NotificationService {
         User currentUser = userService.getCurrentUser();
 
         return repository.findByUserId(currentUser.getId(),pageable);
+    }
+
+    private void sendNotification(String email,String message) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setFrom(username);
+        mailMessage.setTo(email);
+        mailMessage.setSubject("Skideo notification");
+        mailMessage.setText(message);
+
+        mailSender.send(mailMessage);
     }
 
 }
