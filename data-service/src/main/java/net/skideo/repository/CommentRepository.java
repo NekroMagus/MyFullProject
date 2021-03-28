9549ab24fc9d779b3d5f111df5c244f25edecfb7
Opachi <egorodinczov@yandex.ru>
2021-03-28 15:04:17 +0300

    refactoring

--- data-service/src/main/java/net/skideo/repository/CommentRepository.java
+++ data-service/src/main/java/net/skideo/repository/CommentRepository.java
@@ -2,10 +2,12 @@ package net.skideo.repository;
 
 import net.skideo.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
+@Transactional(readOnly = true)
 public interface CommentRepository extends JpaRepository<Comment,Long> {
-
 
     List<Comment> findAll();
 
--- data-service/src/main/java/net/skideo/repository/LikeRepository.java
+++ data-service/src/main/java/net/skideo/repository/LikeRepository.java
@@ -2,9 +2,11 @@ package net.skideo.repository;
 
 import net.skideo.model.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Transactional(readOnly = true)
 public interface LikeRepository extends JpaRepository<Like,Long> {
 
 }
